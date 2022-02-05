10d54abf3a5b3c7e14d92a2c762e4e2afe84bb4a
kimhyerin0909 <khyerin0909@gmail.com>
2022-02-05 15:06:56 +0900

    나도 웹팩 써보자

---
+++ typescript/project1/src/index.js
@@ -0,0 +1,9 @@
+import chalk from './chalk';
+
+console.log(chalk.blue('Hello world!'));
+
+// import chalk from 'chalk';
+
+// const chalk = require('chalk');
+// const message = 'Bundled by the Webpack';
+// console.log(chalk.black.bgGreenBright(message));
---
+++ typescript/project1/src/index.ts
@@ -0,0 +1,3 @@
+function sayHelo(name:string):void {
+    console.log(`Hello! ${name}`);
+}
